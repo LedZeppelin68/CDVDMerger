0c6953e2c8913309ea4d0a808dfefdc63ce1ec79
LedZeppelin68 <ledzeppelin68@gmail.com>
2018-09-30 01:21:36 +0300

    Initial commit

---
+++ CDVDMerger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CDVDMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                string Parent_Name = Path.GetDirectoryName(arg);
+
+                List<string> Clones = GetClones(arg);
+                
+            }
+        }
+
+        private static List<string> GetClones(string arg)
+        {
+            List<string> Clones = new List<string>();
+            Clones.AddRange(Directory.GetDirectories(arg));
+            Clones.AddRange(Directory.GetFiles(arg));
+            return Clones;
+        }
+    }
+}
